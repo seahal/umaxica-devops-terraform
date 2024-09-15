98d2c8abe99cce2f81efa8fb8dd5e966a657f8f5
seahal <seahal.com@gmail.com>
2024-09-15 17:09:36 +0900

    add new file

--- src/index.ts
+++ src/index.ts
@@ -6,4 +6,15 @@ app.get('/', (c) => {
   return c.text(`UMAXICA + ${Date.now() }`)
 })
 
+app.get('/abc', (c) => {
+                return c.text("abc")
+})
+
+app.get('/api/:id', (c) => {
+                    c.header('X-Message', 'hi');
+                    const id = c.req.param('id');
+                    console.log(id);
+                    return c.json({ok: true, message: 'hello, world!', id:  `${id}`});
+})
+
 app.fire()
