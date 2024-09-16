8f5bb14b522fc906f9c337fe336160b0d648c1b4
seahal <seahal.com@gmail.com>
2024-09-16 15:35:53 +0900

    [] start learing.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,18 @@
 import { Hono } from "hono";
-import { secureHeaders } from "hono/secure-headers";
 const app = new Hono();
+import { compress } from "hono/compress";
+import { logger } from "hono/logger";
+import { secureHeaders } from "hono/secure-headers";
+import { testClient } from "hono/testing";
+
 app.use(
     secureHeaders({
         xFrameOptions: "DENY",
         xXssProtection: "1",
     }),
 );
+app.use(compress());
+app.use(logger());
 
 app.get("/", (c) => {
     return c.html(`UMAXICA + ${Date.now()}`);
@@ -14,6 +20,14 @@ app.get("/", (c) => {
 
 app.notFound((c) => {
     return c.html("Custome 404 ???", 404);
+});
+
+app.get("/api", (c) => {
+    return c.json({ message: "hello" });
+});
+
+test('post', async () => {
+    expect(10).toBe(10)
 });
 
 app.fire();
