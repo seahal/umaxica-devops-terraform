942539624696930e3c9ce5dde5a8a51ead810acb
seahal <seahal.com@gmail.com>
2024-09-16 23:43:45 +0900

    Update app.tsx

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,17 @@
-import { Hono } from "hono";
-import { html, raw } from "hono/html";
+import {} from 'hono'
+import {cors} from 'hono/cors'
+import {basicAuth} from 'hono/basic-auth'
+import {prettyJSON} from 'hono/pretty-json'
 
-const app = new Hono();
+type Bindings = {
+    USERNAME: string
+    PASSWORD: string
+}
 
-app.get("/abouts", (c) => {
-	const { username } = c.req.param();
-	return c.html(
-		html`<!doctype html>
-      <h1>Hello! ${username}!</h1>`,
-	);
-});
+const app = new Hono()
+
+app.get('/', (c) => {
+    c.text('aa');
+})
+
+export default app
