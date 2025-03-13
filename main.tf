372acd83801aae9cbeb5fe4e91c43a2fa5f69ca4
seahal <seahal.com@gmail.com>
2025-03-14 03:48:34 +0900

    now, learning terraform.

--- main.tf
+++ main.tf
@@ -33,11 +33,11 @@ resource "aws_s3_bucket" "example" {
   bucket = "umaxica.app"
 }
 
-resource "aws_s3_bucket" "two" {
-  bucket = "my-tf-test-bucket"
+resource "aws_s3_bucket" "cloudfront_functions" {
+  bucket = "umaxica.cloudfront.functions"
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name        = "snipets for cloudfront functions"
+    Environment = "Production"
   }
 }
