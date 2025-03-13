1847275bfffc4bca62d067bd35be5395bad6b6ce
seahal <seahal.com@gmail.com>
2025-03-13 21:57:55 +0900

    [LEARNING] ...

--- provider.tf
+++ provider.tf
@@ -6,6 +6,22 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 5"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "2.36.0"
+    }
+    fastly = {
+      source = "fastly/fastly"
+      version = "5.17.0"
+    }
+ sakuracloud = {
+      source = "sacloud/sakuracloud"
+      version = "2.26.1"
+    }
   }
 }
 
