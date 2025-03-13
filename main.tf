8beb7a7c9a7d62ff309510b1b95d6f8c8593eb6a
seahal <seahal.com@gmail.com>
2025-03-13 23:59:37 +0900

    [LEARNING] ...

--- main.tf
+++ main.tf
@@ -11,15 +11,15 @@ terraform {
       version = "~> 5"
     }
     kubernetes = {
-      source = "hashicorp/kubernetes"
+      source  = "hashicorp/kubernetes"
       version = "2.36.0"
     }
     fastly = {
-      source = "fastly/fastly"
+      source  = "fastly/fastly"
       version = "5.17.0"
     }
- sakuracloud = {
-      source = "sacloud/sakuracloud"
+    sakuracloud = {
+      source  = "sacloud/sakuracloud"
       version = "2.26.1"
     }
   }
@@ -27,4 +27,17 @@ terraform {
 
 provider "aws" {
   region = "ap-northeast-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "umaxica.app"
+}
+
+resource "aws_s3_bucket" "two" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
 }
