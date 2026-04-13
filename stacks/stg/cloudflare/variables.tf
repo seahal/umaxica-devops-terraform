0333a9407b7e8f1130e6bdc5d9845c61dc3e7b81
seahal <m.shiihara@gmail.com>
2026-04-13 22:55:23 +0900

    [update] we could control turnstile and rule on terraform.

--- stacks/stg/cloudflare/variables.tf
+++ stacks/stg/cloudflare/variables.tf
@@ -3,17 +3,3 @@ variable "account_id" {
   type        = string
   default     = "c90999d8a4039c63d02b7a7b1545d211"
 }
-
-variable "cloudflare_workers_builds_repo_connection_uuid" {
-  description = "Cloudflare Workers Builds repo connection UUID"
-  type        = string
-  default     = "5950fe45-4ddd-4b50-bcdd-d0a45efe08a8"
-  nullable    = true
-}
-
-variable "cloudflare_workers_builds_workers" {
-  description = "Worker names to sync Cloudflare Workers Builds for. Null enables all workers."
-  type        = list(string)
-  default     = null
-  nullable    = true
-}
