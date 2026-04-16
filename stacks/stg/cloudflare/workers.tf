a96740d8add8a4663f662daeb44b8654a61c0ea8
seahal <m.shiihara@gmail.com>
2026-04-17 02:43:18 +0900

    [update] daily update

--- stacks/stg/cloudflare/workers.tf
+++ stacks/stg/cloudflare/workers.tf
@@ -7,26 +7,14 @@ locals {
     {
       for zone in ["app", "com", "org"] :
       "umaxica-apps-edge-${zone}-core" => {}
-    },
-    {
-      for zone in ["app", "com", "org"] :
-      "umaxica-apps-edge-${zone}-docs" => {}
-    },
-    {
-      for zone in ["app", "com", "org"] :
-      "umaxica-apps-edge-${zone}-help" => {}
-    },
-    {
-      for zone in ["app", "com", "org"] :
-      "umaxica-apps-edge-${zone}-news" => {}
     }
   )
 
   worker_observability = {
     for name in keys(local.workers) :
     name => {
-      enabled            = name != "umaxica-apps-edge-com-help"
-      head_sampling_rate = name == "umaxica-apps-edge-com-help" ? 1 : 0.2
+      enabled            = false
+      head_sampling_rate = 1
       logs = {
         enabled            = true
         head_sampling_rate = 0.2
@@ -53,8 +41,8 @@ locals {
         "umaxica-apps-edge-app-core",
         "umaxica-apps-edge-com-apex",
         "umaxica-apps-edge-com-core",
-        "umaxica-apps-edge-com-help",
         "umaxica-apps-edge-net-apex",
+        "umaxica-apps-edge-org-core",
       ], name) ? ["cf:environment=production"] : [],
       ["cf:service=${name}"],
     )
@@ -82,10 +70,6 @@ resource "cloudflare_worker" "workers" {
 
   lifecycle {
     prevent_destroy = true
-    ignore_changes = [
-      references,
-      updated_on,
-    ]
   }
 }
 
@@ -99,20 +83,6 @@ import {
   id = "5acaaaa3264e47d789b8443b8925c4ef"
 }
 
-import {
-  to = cloudflare_worker.workers["umaxica-apps-edge-app-docs"]
-  id = "a57aa87c60804ca6ac3fefd580f64a3b"
-}
-
-import {
-  to = cloudflare_worker.workers["umaxica-apps-edge-app-help"]
-  id = "321a87daa8cc4e7e9067f4e5f1ff351a"
-}
-
-import {
-  to = cloudflare_worker.workers["umaxica-apps-edge-app-news"]
-  id = "d55cfb765308440680de7375cada7888"
-}
 
 import {
   to = cloudflare_worker.workers["umaxica-apps-edge-com-apex"]
@@ -124,20 +94,6 @@ import {
   id = "392d8e438e71487083c1cd59ca953b70"
 }
 
-import {
-  to = cloudflare_worker.workers["umaxica-apps-edge-com-docs"]
-  id = "91cfa3c1ce3746f799515cee950f3161"
-}
-
-import {
-  to = cloudflare_worker.workers["umaxica-apps-edge-com-help"]
-  id = "f957280c1a744ed382b1bd6360044136"
-}
-
-import {
-  to = cloudflare_worker.workers["umaxica-apps-edge-com-news"]
-  id = "bcd091417e624f1d8a4f52446d1f81f1"
-}
 
 import {
   to = cloudflare_worker.workers["umaxica-apps-edge-net-apex"]
@@ -154,17 +110,3 @@ import {
   id = "12df970e8b604ad2959d2089c4ff44fd"
 }
 
-import {
-  to = cloudflare_worker.workers["umaxica-apps-edge-org-docs"]
-  id = "8b89830e10024b2cbc1a42ab8327a7cf"
-}
-
-import {
-  to = cloudflare_worker.workers["umaxica-apps-edge-org-help"]
-  id = "296ace2409df4105a2a3578ee59f8c0f"
-}
-
-import {
-  to = cloudflare_worker.workers["umaxica-apps-edge-org-news"]
-  id = "56d9c2b918724f369d806b7afd72b4eb"
-}
